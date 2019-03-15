5bc5315645a186450add16383257698961709289
KimjManansala <kimjmanansala@gmail.com>
2019-03-14 20:34:48 -0500

    Updates

---
+++ precache-manifest.3716d67bd70c0851b74bd456a6d0cc7d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b78cb1241a4da52aa78b86a433526ae0",
+    "url": "/checkers/static/media/navBar.b78cb124.jpg"
+  },
+  {
+    "revision": "3e54790a7581081c4dcae365156293a0",
+    "url": "/checkers/static/media/king.3e54790a.png"
+  },
+  {
+    "revision": "a032e906edffedb80c9156a60fd6eec0",
+    "url": "/checkers/static/media/download.a032e906.jpeg"
+  },
+  {
+    "revision": "840ce03b29508c38e849",
+    "url": "/checkers/static/js/runtime~main.840ce03b.js"
+  },
+  {
+    "revision": "cb7f64bbc3c88e5878dd",
+    "url": "/checkers/static/js/main.cb7f64bb.chunk.js"
+  },
+  {
+    "revision": "cf8bc514bf1da749e617",
+    "url": "/checkers/static/js/2.cf8bc514.chunk.js"
+  },
+  {
+    "revision": "cb7f64bbc3c88e5878dd",
+    "url": "/checkers/static/css/main.357f3f12.chunk.css"
+  },
+  {
+    "revision": "0ed43af02f765ba0d18a335b8de81546",
+    "url": "/checkers/index.html"
+  }
+];
